0757276bf25d78f992279f5e8747fc76ae1d82db
BrunoSan <Bruno-kk@hotmail.com>
2021-10-18 17:34:34 -0300

    service adicionado/painel criado

--- src/App.js
+++ src/App.js
@@ -1,9 +1,37 @@
-import React from 'react'
+/* eslint-disable no-unused-vars */
+
+/* eslint-disable react/display-name */
+
+import React, {useEffect,useState} from 'react'
+import  {MovieRow}  from './componets/MovieRow'
+import tmDB from './utils/tmDB'
+import './App.css'
+
+
 
 export default () => {
+
+  const[movieList,setMovieList]=useState([])
+
+
+  useEffect(() => {
+    const loadALL = async () => {
+      const list = await tmDB.getHomeList();
+      setMovieList(list)
+    }
+
+    loadALL()
+},[])
   return (
-    <div>
-      <h1>Hello</h1>
+    <div className="page">
+      <section className="listas">
+        {movieList.map((item,key)=>(
+       
+          
+          <MovieRow key={key} title={item.title} items={item.items}/>
+
+        ))}
+      </section>
       
     </div>
   )
--- src/utils/tmDB.js
+++ src/utils/tmDB.js
@@ -1,80 +1,82 @@
-const API_KEY='d7a7b9d8cf4c160d60fad162c3933b38';
-const API_URL='https://api.themoviedb.org/3';
+
+
+
+const API_KEY = 'd7a7b9d8cf4c160d60fad162c3933b38',
+ API_URL = 'https://api.themoviedb.org/3',
 
 
 /**
-     - originais da netflix
-     -recomendados para voce
-     - am alta
-     - ação
-     - comédia
-     - terror
-     - drama
-     - romance
-     - documentarios
-     */
+ *   - originais da netflix
+ *   -recomendados para voce
+ *   - am alta
+ *   - ação
+ *   - comédia
+ *   - terror
+ *   - drama
+ *   - romance
+ *   - documentarios
+ */
 
-const basicFetch = async (endpoint)=>{
-    const req= await fetch(`${API_URL}${endpoint}`);
-    const json = await req.json();
+ basicFetch = async (endpoint) => {
+    const req = await fetch(`${API_URL}${endpoint}`),
+     json = await req.json();
     return json;
 
 }    
     
 
-export default{
-    getHomeList: async ()=>{
-        return[
+export default {
+    'getHomeList': async () => {
+        return [
             {
-                slug:'originals',
-                title:'Originais da Netflix',
-                items:await basicFetch(`/discover/tv?with_network=213&language=pt-BR&api_key=${API_KEY}`)
+                slug: 'originals',
+                title: 'Originais da Netflix',
+                items: await basicFetch(`/discover/tv?with_network=213&language=pt-BR&api_key=${API_KEY}`)
             },
             {
-                slug:'trending',
-                title:'recomendados para voce',
-                items:[]
+                slug: 'trending',
+                title: 'recomendados para voce',
+                items: await basicFetch(`/trending/all/week?&language=pt-BR&api_key=${API_KEY}`)
 
             },
             {
-                slug:'toptraded',
-                title:'Em alta',
-                items:[]
+                slug: 'toptraded',
+                title: 'Em alta',
+                items: await basicFetch(`/movie/top_rated?language=pt-BR&api_key=${API_KEY}`)
             },
             {
-                slug:'action',
-                title:'Ação',
-                items:[]
+                slug: 'action',
+                title: 'Ação',
+                items: await basicFetch(`/discover/movie?with_genres=28&language=pt-BR&api_key=${API_KEY}`)
             },
             {
-                slug:'comedy',
-                title:'Comédia',
-                items:[]
+                slug: 'comedy',
+                title: 'Comédia',
+                items: await basicFetch(`/discover/movie?with_genres=35&language=pt-BR&api_key=${API_KEY}`)
             },
             {
-                slug:'terror',
-                title:'Terror',
-                items:[]
+                slug: 'terror',
+                title: 'Terror',
+                items: await basicFetch(`/discover/movie?with_genres=27&language=pt-BR&api_key=${API_KEY}`)
             },
             {
-                slug:'drama',
-                title:'Drama',
-                items:[]
+                slug: 'drama',
+                title: 'Drama',
+                items: await basicFetch(`/discover/movie?with_genres=18&language=pt-BR&api_key=${API_KEY}`)
             },
             {
-                slug:'romance',
-                title:'Romance',
-                items:[]
+                slug: 'romance',
+                title: 'Romance',
+                items: await basicFetch(`/discover/movie?with_genres=10749&language=pt-BR&api_key=${API_KEY}`)
             },
             {
-                slug:'documentary',
-                title:'Documentário',
-                items:[]
-            },
+                slug: 'documentary',
+                title: 'Documentário',
+                items: await basicFetch(`/discover/movie?with_genres=99&language=pt-BR&api_key=${API_KEY}`)
+            }
 
         ]
     }
-    
 
 
 }
